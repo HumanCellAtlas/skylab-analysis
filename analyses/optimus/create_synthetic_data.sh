ed8ee48f1d39f393c17ada8c5f9316372c0681e3
Ambrose J. Carr <mail@ambrosejcarr.com>
2018-05-09 23:27:19 -0400

    addressed comments

--- analyses/optimus/create_synthetic_data.sh
+++ analyses/optimus/create_synthetic_data.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-
-# this script takes as input the PBMC8k dataset, multi-aligned with Optimus.
-# it generates a bam file containing unique, unaligned, and multialigned reads from different
-# chromosomes
-# it then regenerates fastq data
+# This script:
+# Takes an Optimus multi-aligned bam file as input
+# Extracts aligned reads from chr1, multialigned reads from chr2, and unaligned reads from the input bam
+# Reverts the extracted data to fastq and exits.
+# These inputs are then run through Optimus and Cell Ranger as part of a pending scientific testing framework.
 
 set -eo pipefail
 
@@ -14,10 +14,11 @@ curl -o picard.jar https://github.com/broadinstitute/picard/releases/download/2.
 # suo apt-get install samtools
 # sudo yum install samtools
 
-INPUT_BAM=$1  # pbmc8k resequenced output
-N_UNIQUE=$2  # 1,000,000
-N_UNALIGNED=$3  # 100,000
-N_MULTIALIGNED=$4  # 100,000
+INPUT_BAM=$1  # bam file input from Optimus, HCA uses the pbmc8k dataset found here:
+# http://s3-us-west-2.amazonaws.com/10x.files/samples/cell-exp/2.1.0/pbmc8k/pbmc8k_fastqs.tar
+N_UNIQUE=${2:-1000000}
+N_UNALIGNED=${3:-100000}
+N_MULTIALIGNED=${4:-100000}
 
 # sort and index the bam file
 samtools sort -o sorted.bam ${INPUT_BAM}
@@ -31,6 +32,9 @@ samtools view sorted.bam 1 | head -n ${N_UNIQUE} > unique.sam
 samtools view -f 4 sorted.bam | head -n "${N_UNALIGNED}" > unaligned.sam
 
 # get some multi-aligned reads
+# the awk command here retrieves records that were aligned between 2-9 times, but then extracts
+# only the primary records (HI:i:1) because we're converting this back to fastq, so we don't
+# need the alternative alignment locations.
 samtools view sorted.bam 2 |\
   awk '/NH:i:[2-9]/&&/HI:i:1/' |\
   head -n "${N_MULTIALIGNED}" > multialigned.sam
@@ -52,3 +56,4 @@ mv CR_UR.fastq.gz r1.fastq.gz
 
 # all done
 echo "finished subsetting $2 unique, $3 unaligned and $4 multi-aligned reads from $1"
+
