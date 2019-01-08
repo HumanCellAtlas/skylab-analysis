73180c5d0c66ef7fb40f68f289e14e84d7dd87c3
Nick Barkas <nikolas.barkas@outlook.com>
2019-01-08 09:59:07 -0500

    parametrize test_emptyDropsWrapper.sh

--- tools/emptyDropsWrapper/test/test_emptyDropsWrapper.sh
+++ tools/emptyDropsWrapper/test/test_emptyDropsWrapper.sh
@@ -2,24 +2,32 @@
 
 ## Description: Unit test for emptyDropsWrapper script
 
+## Test parameters
+testDataURL=http://cf.10xgenomics.com/samples/cell-exp/2.1.0/pbmc4k/pbmc4k_raw_gene_bc_matrices.tar.gz
+testDataFileName=pbmc4k_raw_gene_bc_matrices.tar.gz
+testDataInputMatrixPath=raw_gene_bc_matrices/GRCh38
+rdsFileName=pbmc4k.rds
+emptyDropsOutput=pbmc4k_emptyDrops.csv
+md5checksum="c2b0c8f24b9f8383f7de710774c9bbab"
+
 ## Download some sample data from 10X
 printf "Downloading pbmc4k data..."
-wget -q http://cf.10xgenomics.com/samples/cell-exp/2.1.0/pbmc4k/pbmc4k_raw_gene_bc_matrices.tar.gz
+wget -q $testDataURL
 printf "done\n"
 
 ## Decompress dataset
 printf "Decompressing archive..."
-tar xzf pbmc4k_raw_gene_bc_matrices.tar.gz
+tar xzf $testDataFileName
 printf "done\n"
 
 ## Prepare RDS files for reading from the the 10X matrix
 printf "Converting mtx to rds..."
-./prepRDS.R
+./prepRDS.R --input ${testDataInputMatrixPath} --output ${rdsFileName}
 printf "done\n"
 
 ## Run empty drops
 printf "Running emptyDrops..."
-../emptyDropsWrapper.R -i pbmc4k.rds -o pbmc4k_emptyDrops.csv
+../emptyDropsWrapper.R -i ${rdsFileName} -o ${emptyDropsOutput}
 printf "done\n"
 
 ## Check the output md5 checksum
@@ -28,8 +36,8 @@ printf "done\n"
 ## test it was and therefore the following checksum always
 ## validates
 printf "Verifying checksum..."
-md5out=`md5sum pbmc4k_emptyDrops.csv | cut -f 1 -d ' '`
-if [ "$md5out" = "c2b0c8f24b9f8383f7de710774c9bbab" ];
+md5out=`md5sum ${emptyDropsOutput} | cut -f 1 -d ' '`
+if [ "$md5out" = "$md5checksum" ];
 then
     echo 'PASSED'
 else
@@ -38,4 +46,9 @@ fi
 printf "done\n"
 
 ## Cleanup
-rm -r pbmc4k_emptyDrops.csv pbmc4k_raw_gene_bc_matrices.tar.gz raw_gene_bc_matrices pbmc4k.rds
+
+rm -r raw_gene_bc_matrices # Not sure how to do this safely
+rm -r ${emptyDropsOutput} ${testDataFileName} $ ${rdsFileName}
+
+
+
